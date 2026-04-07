748ae404ce15b486ca1a91e7f79a2f47e0e1944f
johnboscochukwuemeka38-max <johnboscochukwuemeka38@gmail.com>
2026-04-07 15:34:27 +0100

    .

--- expense-track/src/components/Info.jsx
+++ expense-track/src/components/Info.jsx
@@ -17,7 +17,7 @@ const Info = ({title, subTitle}) => {
                 <input type="text" placeholder='Search now...' className='outline-none text-black dark:text-slate-400'/>
             </div>
 
-            <div className='px-4 py-2 flex flex-row items-center bg-violet-800 text-white rounded-md gap-2 w-40'>
+            <div className='py-2 flex flex-row items-center justify-center bg-violet-800 text-white rounded-md gap-2 w-35'>
                 <IoFilter size={24}/>
                 <p className='text-base'>Filter By</p>
             </div>
